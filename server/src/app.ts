85b1cded7ef1cef55e4b74ca4a62f6569e5eb176
Obafemi Lawal <swissbobo@gmail.com>
2023-01-14 13:10:52 +0300

    done

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import urlsRouteV1 from "./route/urlsRouteV1";
+import cors from "cors";
 
 const router = express.Router();
 
 const app = express();
 app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:8000",
+    })
+);
 app.use("/", router);
 
 urlsRouteV1(router);
